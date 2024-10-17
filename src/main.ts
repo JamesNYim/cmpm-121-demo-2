1ab2a71dcd5ac152ae8af19491b38e6d9e95eee5
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2024-10-17 15:51:30 -0700

    Step 2 In Progress: added canvas drawing

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,43 @@ const appTitle = document.createElement("h1");
 appTitle.textContent = APP_NAME;
 app.append(appTitle);
 
+// Canvas
 const artCanvas = document.createElement("canvas");
 artCanvas.id = 'artCanvas';
 artCanvas.height = 256;
 artCanvas.width = 256;
+app.append(artCanvas);
+
+// Mouse Events
+let drawing = false;
+const startDrawing = (event: MouseEvent) => {
+    drawing = true;
+    draw(event);
+};
+
 const ctx = artCanvas.getContext("2d");
+if (!ctx) {
+    throw new Error('Failed to retrieve 2d context from canvas.');
+}
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, 256, 256);
-app.append(artCanvas);
+const draw = (event: MouseEvent) => {
+    if (!drawing)
+        return;
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
+};
+
+const stopDrawing = () => {
+    drawing = false;
+};
+
+// Register the mouse event listeners on the canvas
+artCanvas.addEventListener('mousedown', startDrawing);
+artCanvas.addEventListener('mousemove', draw);
+artCanvas.addEventListener('mouseup', stopDrawing);
+artCanvas.addEventListener('mouseleave', stopDrawing);
