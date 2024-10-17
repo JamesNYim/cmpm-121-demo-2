9df8da0bf34929dd41730c1091ad0ecdf043c669
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2024-10-17 14:58:03 -0700

    Step 1 Complete: Added title and canvas

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,17 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+// App Title Header
+const appTitle = document.createElement("h1");
+appTitle.textContent = APP_NAME;
+app.append(appTitle);
+
+const artCanvas = document.createElement("canvas");
+artCanvas.id = 'artCanvas';
+artCanvas.height = 256;
+artCanvas.width = 256;
+const ctx = artCanvas.getContext("2d");
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 256, 256);
+app.append(artCanvas);
